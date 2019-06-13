2255a6d7d7e5ac0006268e281531fe4fedcde984
Ferdysan <ferdysan87@gmail.com>
2019-06-13 21:33:30 +0200

    correzione

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   $password = $_GET['password'];
 
   if(strpos($password, 'Boolean') !== false || strpos($password, 'boolean') !== false ){
-    echo '<div style="color:green;">ACCESSO CONSENTITO</div>';
+    echo '<div style="color:green; font-weight:bold;">ACCESSO CONSENTITO</div>';
   }else{
     echo '<div style="color:red;">ACCESSO NEGATO</div>';
   }
